ef53fc71e0ad95cfa8e72a63f15e1c6aa38091de
Смердокрыл <smerdokryl@gmail.com>
2020-12-16 22:26:45 +0200

    ''

---
+++ SDK/samples/Pools/script.cpp
@@ -0,0 +1,26 @@
+/*
+	THIS FILE IS A PART OF RDR 2 SCRIPT HOOK SDK
+				http://dev-c.com
+			(C) Alexander Blade 2019
+*/
+
+#include "script.h"
+#include <string>
+#include <vector>
+
+void update() {}
+
+void main()
+{		
+	while (true)
+	{
+		update();
+		WAIT(0);
+	}
+}
+
+void ScriptMain()
+{	
+	srand(GetTickCount());
+	main();
+}
